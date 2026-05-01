b5c66f60c7d8de103deeec80cb90659a563c5655
metaironia <serezha364@gmail.com>
2026-05-01 21:10:08 +0300

    fixed testbench

--- problems/2025/00_signext/signext_tb.v
+++ problems/2025/00_signext/signext_tb.v
@@ -6,19 +6,25 @@ localparam N = 12;
 localparam M = 32;
 
 reg  [N-1:0] in  = {N{1'b0}};
+reg  has_error = 0;
 wire [M-1:0] out;
 
 always begin
     #1 if (out[N-1:0] == in[N-1:0]) begin
         if (out[M-1:N] == {(M-N){in[N-1]}})
             $display("[%t] in=%b, out=%b, OK", $realtime, in, out);
-        else
+        else begin
+            has_error = 1;
             $display("[%t] in=%b, out=%b, FAIL", $realtime, in, out);
         end
+    end
     #1 in = in + 1;
     if (in == 0) begin
-       $display("[%t] Done", $realtime);
-       $finish;
+        if (has_error)
+            $display("[%t] Tests failed", $realtime);
+        else
+            $display("[%t] Tests passed", $realtime);
+        $finish;
     end
 end
 
