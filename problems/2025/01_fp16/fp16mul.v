9f7b79b69ff9db099618ba5c40b16b68201e2fa9
metaironia <serezha364@gmail.com>
2026-05-07 04:14:59 +0300

    implemented fp16mul, need to be tested

--- problems/2025/01_fp16/fp16mul.v
+++ problems/2025/01_fp16/fp16mul.v
@@ -10,66 +10,94 @@ module fp16mul #(
 );
 
 localparam EXP_MANT_BITS = EXP_BITS + MANT_BITS;
+localparam DOUBLE_MANT_SIZE = (MANT_BITS + 1) * 2;
 
 function get_sign_bit;
-    input [EXP_MANT_BITS:0] fp_16;
+    input [EXP_MANT_BITS:0] fp16;
     begin
         get_sign_bit = fp16[EXP_MANT_BITS];
     end
 endfunction
 
-function get_exponent_bits;
-    input [EXP_MANT_BITS:0] fp_16;
+function [EXP_MANT_BITS-1:MANT_BITS] get_exponent_bits;
+    input [EXP_MANT_BITS:0] fp16;
     begin
         get_exponent_bits = fp16[EXP_MANT_BITS-1:MANT_BITS];
     end
 endfunction
 
-function get_mantissa_bits;
-    input [EXP_MANT_BITS:0] fp_16;
+function [MANT_BITS-1:0] get_mantissa_bits;
+    input [EXP_MANT_BITS:0] fp16;
     begin
         get_mantissa_bits = fp16[MANT_BITS-1:0];
     end
 endfunction
 
-assign o_res[EXP_MANT_BITS] = get_sign_bit(i_a) ^ get_sign_bit(i_b);
-
 wire is_initial_inf;
 wire is_inf_after_norm;
 
-wire initial_exp_sum;
-wire exp_sum_after_norm;
+wire [EXP_BITS-1:0] initial_exp_sum;
+wire [EXP_BITS-1:0] exp_sum_after_norm;
 
-reg [2*(MANT_BITS+1)-1:0] mant_mul_res;
+wire [EXP_MANT_BITS:0] i_a_das;
+wire [EXP_MANT_BITS:0] i_b_das;
+
+wire is_a_nan;
+wire is_b_nan;
 
 wire [MANT_BITS:0] full_mant_a;
-wire [MANT_BITS:0] full_mant_b; 
+wire [MANT_BITS:0] full_mant_b;
 
-assign full_mant_a = {1'b1, get_mantissa_bits(i_a)}; 
-assign full_mant_b = {1'b1, get_mantissa_bits(i_b)}; 
+assign full_mant_a = {1'b1, get_mantissa_bits(i_a_das)}; 
+assign full_mant_b = {1'b1, get_mantissa_bits(i_b_das)}; 
+
+wire [DOUBLE_MANT_SIZE-1:0] mant_mul_res;
+wire [DOUBLE_MANT_SIZE-1:0] mant_mul_after_norm;
+wire [MANT_BITS-1:0] mant_mul_after_round;
+
+assign mant_mul_res = full_mant_a * full_mant_b;
 
 // rounding towards zero
 always @(*) begin
-    mant_mul_res = full_mant_a * full_mant_b;
-    if (mant_mul_res[2*(MANT_BITS+1)-1] == 1'b1)
-        o_res[MANT_BITS:0] = mant_mul_res[2*(MANT_BITS+1)-2:2*(MANT_BITS+1)-1];
-
+    o_res[EXP_MANT_BITS] = get_sign_bit(i_a) ^ get_sign_bit(i_b);
+    
+    if (is_a_nan || is_b_nan)
+        o_res[EXP_MANT_BITS-1:0] = {{(EXP_BITS){1'b1}}, {(MANT_BITS-1){1'b0}}, {1'b1}}; 
+    else if (is_inf_after_norm)
+        o_res[EXP_MANT_BITS-1:0] = {{(EXP_BITS){1'b1}}, {(MANT_BITS){1'b0}}};
+    else
+        o_res[EXP_MANT_BITS-1:0] = {exp_sum_after_norm, mant_mul_after_round};
 end
 
-exponent_adder #(.EXP_BITS(EXP_BITS)) initial_exp_adder_inst(.i_exp_a(get_exponent_bits(i_a)),
-                                                             .i_exp_b(get_exponent_bits(i_b)),
+is_nan #(.EXP_BITS(EXP_BITS), .MANT_BITS(MANT_BITS)) is_in_a_nan_inst(.i_a(i_a),
+                                                                      .o_is_nan(is_a_nan));
+
+is_nan #(.EXP_BITS(EXP_BITS), .MANT_BITS(MANT_BITS)) is_in_b_nan_inst(.i_a(i_b),
+                                                                      .o_is_nan(is_b_nan));
+
+denormal_as_zero #(.EXP_BITS(EXP_BITS), .MANT_BITS(MANT_BITS)) get_das_in_a_inst(.i_a(i_a),
+                                                                                 .o_res(i_a_das));
+
+denormal_as_zero #(.EXP_BITS(EXP_BITS), .MANT_BITS(MANT_BITS)) get_das_in_b_inst(.i_a(i_b),
+                                                                                 .o_res(i_b_das));
+
+exponent_adder #(.EXP_BITS(EXP_BITS)) initial_exp_adder_inst(.i_exp_a(get_exponent_bits(i_a_das)),
+                                                             .i_exp_b(get_exponent_bits(i_b_das)),
                                                              .o_res_exp(initial_exp_sum),
-                                                             .o_is_inf(is_initial_inf))
+                                                             .o_is_inf(is_initial_inf));
 
 normalize_mantissa_after_mul #(.MANT_BITS(MANT_BITS)) norm_mant_inst(.i_mant(mant_mul_res),
                                                                      .o_mant(mant_mul_after_norm),
-                                                                     .o_is_shifted(is_mant_shifted_after_norm)));
+                                                                     .o_is_shifted(is_mant_shifted_after_norm));
 
 exponent_updater #(.EXP_BITS(EXP_BITS)) exp_updater_after_norm_inst(.i_exp_a(initial_exp_sum),
-                                                                    .i_update(mant_mul_res[2*(MANT_BITS+1)-1]),
+                                                                    .i_update(is_mant_shifted_after_norm),
                                                                     .i_is_inf(is_initial_inf),
                                                                     .o_res_exp(exp_sum_after_norm),
                                                                     .o_is_inf(is_inf_after_norm));
+
+round_toward_zero #(.MANT_BITS(MANT_BITS)) round_mant_after_norm_inst(.i_mant(mant_mul_after_norm),
+                                                                      .o_mant(mant_mul_after_round));
 
 
 endmodule
--- problems/2025/01_fp16/fp16operations.v
+++ problems/2025/01_fp16/fp16operations.v
@@ -1,10 +1,41 @@
+module round_toward_zero #(
+    parameter MANT_BITS = 10
+)(
+    input  wire [DOUBLE_MANT_SIZE-1:0] i_mant,
+    output reg  [MANT_BITS-1:0]        o_mant
+);
+
+localparam DOUBLE_MANT_SIZE = (MANT_BITS + 1) * 2;
+
+always @(*) begin
+    o_mant = i_mant[DOUBLE_MANT_SIZE-3:MANT_BITS];
+end 
+
+endmodule
+
+module is_nan #(
+    parameter EXP_BITS = 5,
+    parameter MANT_BITS = 10
+)(
+    input  wire [EXP_MANT_BITS:0] i_a,
+    output reg                    o_is_nan
+);
+
+localparam EXP_MANT_BITS = EXP_BITS + MANT_BITS;
+
+always @(*) begin
+    o_is_nan = |i_a[EXP_MANT_BITS-1:MANT_BITS] & |i_a[MANT_BITS-1:0];
+end
+
+endmodule
+
 module exponent_adder #(
     parameter EXP_BITS = 5
 )(
-    input wire [EXP_BITS-1:0] i_exp_a,
-    input wire [EXP_BITS-1:0] i_exp_b,
-    output reg [EXP_BITS-1:0] o_res_exp,
-    output reg o_is_inf
+    input  wire [EXP_BITS-1:0] i_exp_a,
+    input  wire [EXP_BITS-1:0] i_exp_b,
+    output reg  [EXP_BITS-1:0] o_res_exp,
+    output reg                 o_is_inf
 );
 
 reg [EXP_BITS:0] tmp_exp;
@@ -26,11 +57,11 @@ endmodule
 module exponent_updater #(
     parameter EXP_BITS = 5
 )(
-    input wire [EXP_BITS-1:0] i_exp_a,
-    input wire i_update,
-    input wire i_is_inf,
-    output reg [EXP_BITS-1:0] o_res_exp,
-    output reg o_is_inf
+    input  wire [EXP_BITS-1:0] i_exp_a,
+    input  wire                i_update,
+    input  wire                i_is_inf,
+    output reg  [EXP_BITS-1:0] o_res_exp,
+    output reg                 o_is_inf
 );
 
 wire exp_adder_is_inf;
@@ -74,13 +105,15 @@ endmodule
 module normalize_mantissa_after_mul #(
     parameter MANT_BITS = 10
 )(
-    input wire [((EXP_BITS+1)*2)-1:0] i_mant,
-    output reg [((EXP_BITS+1)*2)-1:0] o_mant,
+    input wire [DOUBLE_MANT_SIZE-1:0] i_mant,
+    output reg [DOUBLE_MANT_SIZE-1:0] o_mant,
     output reg o_is_shifted
 );
 
+localparam DOUBLE_MANT_SIZE = (MANT_BITS+1) * 2;
+
 always @(*) begin
-    if (i_mant[((EXP_BITS+1)*2)-1] == 1'b1) begin
+    if (i_mant[DOUBLE_MANT_SIZE-1] == 1'b1) begin
         o_mant = i_mant >> 1;
         o_is_shifted = 1'b1;
     end
